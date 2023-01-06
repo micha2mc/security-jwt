9fda234cf3ba9ea203ef9f295f522c8f00f5f251
MichaMicha <micha2mc@gmail.com>
2023-01-06 13:28:13 +0100

    Probando los repositorios

--- src/main/java/com/projahcode/pjc/demo/controller/DemoController.java
+++ src/main/java/com/projahcode/pjc/demo/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.projahcode.pjc.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/demo-controller")
+@Slf4j
 public class DemoController {
 
     @GetMapping
